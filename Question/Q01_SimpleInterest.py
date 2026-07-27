47a59015198fb03b67e492e5568fa56ddb2bfe29
STYMM <satyamgupta776683@gmail.com>
2026-07-27 20:29:53 +0530

    Updated simple interest program

--- Question/Q01_SimpleInterest.py
+++ Question/Q01_SimpleInterest.py
@@ -1,11 +1,11 @@
 # Write a python program to calculate the simple interest 
-principle = float(input("Enter Your Priciple : "))
+principal = float(input("Enter Your Pricipal : "))
 rate = float(input("Enter Your Rate : "))
 time = float(input("Enter Yout Time "))
 
 SimpleInterst = (principle * rate * time)/100
 print(SimpleInterst)
 
-totalAmount = SimpleInterst + principle
+totalAmount = SimpleInterst + principal
 
 print("Simple Interest " , totalAmount)
