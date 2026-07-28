abb460ee7687bcbe43a2c8a809aeccab2f20c8ef
STYMM <satyamgupta776683@gmail.com>
2026-07-28 20:28:00 +0530

    Improved simple interest program

---
+++ Practice/BASIC/15.py
@@ -0,0 +1,3 @@
+# Write a python code to find the ASCII.
+ascii = input("Enter any character : ")
+print(ord(ascii))
---
+++ Practice/BASIC/16.py
@@ -0,0 +1,4 @@
+# Generate a random number
+import random
+random_int = random.randint(1,100)
+print(random_int)
