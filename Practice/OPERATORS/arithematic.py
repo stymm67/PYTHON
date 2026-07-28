82579778a4ab2e6a9ba7640d2d81eca5eccd7127
STYMM <satyamgupta776683@gmail.com>
2026-07-28 20:35:22 +0530

    Improved simple interest program

---
+++ Practice/OPERATORS/arithematic.py
@@ -0,0 +1,34 @@
+# Arithematic Operator
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+# ADDITION
+add = num1 + num2
+print(add)
+
+# SUBTRACTION 
+sub = num1 - num2 
+print(sub)
+
+# MULTIPLICATION
+mul = num1 * num2 
+print(mul)
+
+# DIVISION 
+div = num1 / num2
+print(div)
+
+
+#FLOOR DIVISION 
+flr_div = num1//num2
+print(flr_div)
+
+
+# MODULUS
+mod = num1 % num2 
+print(mod)
+
+# EXPONENTIATION OPERATOR (POWER)
+exp = num1 ** num2 
+print(exp)
