0194c1c3ecc540e9136388955ead5dda47dcf9f3
STYMM <satyamgupta776683@gmail.com>
2026-07-28 13:44:06 +0530

    Improved simple interest program

---
+++ Practice/eleven.py
@@ -0,0 +1,4 @@
+# Print number from 1 to 1000 but only even number.
+for i in range(100 , 1 , -1): # 100 is starting range  1 is ending range and -1 is jumping range
+  if i%2==0:
+    print(i)
---
+++ Question/secure.py
@@ -0,0 +1,5 @@
+url = "https://google.com"
+print(url.startswith("https"))
+
+email = "satyamgupta776683@gmial.com"
+print(email.endswith(".com"))
