91b3019a2cddf5141e75531503acd3e3fa96964d
STYMM <satyamgupta776683@gmail.com>
2026-07-29 20:57:55 +0530

    Improved simple interest program

--- Practice/OPERATORS/camparision.py
+++ Practice/OPERATORS/camparision.py
@@ -1,1 +1,33 @@
 # CAMPARISION OPERATOR
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter another number : "))
+
+
+# EQUAL TO ==
+if(num1==num2):
+  print("Both are equal" , num1 )
+
+
+# NOT EQUAL TOO
+if(num1!=num2):
+  print("num1 is not equal to num2")
+
+
+# Greater than 
+if(num1<num2):
+  print("num2 is greater" , num2 )
+
+
+# Less than
+if(num1>num2):
+  print("num2 is less than num1" , num1 )
+
+
+# Greater than equal to 
+if(num1>=num2):
+  print("num1 is grater than equal to " , num1)
+
+
+#Less than eqaul to
+if(num1<=num2):
+  print("num1 is less than equal to ", num1)
